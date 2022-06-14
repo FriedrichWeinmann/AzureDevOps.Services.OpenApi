2495b2cb9b00be93c007f2b86153b6d3dfe8a625
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2022-06-14 08:44:11 +0200

    1.0.1

--- AzureDevOps.Services.OpenApi/internal/scripts/initialize.ps1
+++ AzureDevOps.Services.OpenApi/internal/scripts/initialize.ps1
@@ -1,2 +1,4 @@
 ﻿# Define the default service to use when executing requests
 $PSDefaultParameterValues['Invoke-RestRequest:Service'] = 'AzureDevOps'
+
+Set-RestServiceMetadata -Service 'AzureDevOps' -NotConnectedMessage 'Not connected yet! Use Connect-AdsService to establish a connection first.'
